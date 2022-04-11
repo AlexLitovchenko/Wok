15cf9d545e701af3689c595fbb821d77220de125
AlexLitovchenko <e22l16h@mail.ru>
2022-04-11 13:40:57 +0300

    SomethingDone

--- ServiceB/main.go
+++ ServiceB/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	handler "ServiceB/Handler"
 
+	"github.com/AlexLitovchenko/Wok/ServiceB/Handler"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	//handler.Connection()
 	router := gin.Default()
 	router.GET("/A", handler.GetAll)
 	router.Run(":8081")
